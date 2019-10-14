24c8f2be9090a73234dcb062689850f6bad69904
DanielZini <daniel.zini@hotmail.com>
2019-10-14 13:33:04 -0300

    update port

--- index.js
+++ index.js
@@ -19,6 +19,6 @@ app.use(express.static('public/upload'));
 //     res.status(200).send('Meu Backend');
 // })
 
-app.listen(3000, () =>{
+app.listen(process.env.PORT || 3000, () =>{
      console.log('Backend foi');
 })
