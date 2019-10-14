42deb26ec831bd22abfd0e063134fa21937423cb
DanielZini <daniel.zini@hotmail.com>
2019-10-14 13:53:14 -0300

    arruma conexao banco

--- knexfile.js
+++ knexfile.js
@@ -1,15 +1,15 @@
 // Update with your config settings.
-const env = require('./.env');
+// const env = require('./.env');
 
 module.exports = {
 
 
 	client: 'postgresql',
 	connection: {
-		host: 		env.DATABASE_URL,
-		database:	env.DATABASE_NAME,
-		user:    	env.DATABASE_USER,
-		password:	env.DATABASE_PASSWORD
+		host: 		process.env.DATABASE_URL,
+		database:	process.env.DATABASE_NAME,
+		user:    	process.env.DATABASE_USER,
+		password:	process.env.DATABASE_PASSWORD
 	},
 	pool: {
 		min: 2,
